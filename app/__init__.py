2aadacac1b9e2142b16c38538aaa6136ab5c6b99
flammaVitea <bogdan17009@gmail.com>
2025-11-02 17:54:42 +0200

    Add initial Flask application structure with templates and routing
    - Created base HTML template with Bootstrap integration.
    - Added contacts and resume pages with respective templates.
    - Implemented navigation and footer includes for consistent layout.
    - Developed views for main, contacts, and resume routes.
    - Established user blueprint with a greeting page and admin redirect.
    - Included configuration and run scripts for application execution.

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_pyfile('../config.py')
+
+from . import views
+
+from users import users_bp 
+app.register_blueprint(users_bp)
---
+++ users/__init__.py
@@ -0,0 +1,10 @@
+
+from flask import Blueprint
+
+users_bp = Blueprint('users',
+                    __name__,
+                    url_prefix='/users',
+                    template_folder='templates/users'
+                    )
+
+from . import views
